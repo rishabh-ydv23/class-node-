61283965ce8366e6452fe469871dc164777592fb
Rishabh <rishabhyadavunnao@gmail.com>
2025-10-16 23:17:28 +0530

    add some theory which i revised

--- pathm.js
+++ pathm.js
@@ -2,13 +2,31 @@
 // console.log(__filename);
 
 const path1=require("path");
-// console.log(path1);
-
 const filepath=path1.join("folder","student","hello.txt");
 // console.log(filepath);
+// console.log(path1);             
+
+/*
+    MODULE : In Node.js, a module is simply a reusable block of code — a file or set of files — that you can import and use in another file.
+
+    PATH:  is a built-in core module that provides utilities for working with file and directory paths.
+    The variable path now gives you access to various functions to handle and manipulate file paths in a platform-independent way (works both on Windows and Linux).
+
+*/
 
 // console.log(module);
-// console.log(process);               //how the prject is done like complete information execution steps are here 
+    /*
+    In Node.js, module is a special object available in every file. It represents the current module (file).
+    saari properties show hote hai uss file ke 
+    It shows all the “properties” of that file, like:
+        Its path (filename, path)
+        What it exports (exports)
+        Which module required it (parent)
+        Which modules it requires (children)
+        Whether it’s fully loaded (loaded)
+    */
+
+console.log(process);               //how the prject is done like complete information execution steps are here 
 
 
 const parsedata=path1.parse(filepath);
@@ -17,8 +35,8 @@ const dirdata=path1.dirname (filepath);
 const basedata=path1.basename(filepath);
 const extdata=path1.extname(filepath);
 
-console.log(parsedata);
-console.log(resolvedata);
-console.log(dirdata);
-console.log(basedata);
-console.log(extdata);
+// console.log(parsedata);
+// console.log(resolvedata);
+// console.log(dirdata);
+// console.log(basedata);
+// console.log(extdata);
