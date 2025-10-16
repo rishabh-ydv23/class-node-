a3fb842bc6af0f4009cb58042b11826e2184bf49
Rishabh <rishabhyadavunnao@gmail.com>
2025-10-16 16:59:09 +0530

    basic directory strct of node js

---
+++ pathm.js
@@ -0,0 +1,18 @@
+// console.log(__dirname);
+// console.log(__filename);
+
+const path1=require("path");
+// console.log(path1);
+
+const filepath=path1.join("folder","student","hello.txt");
+// console.log(filepath);
+
+// console.log(module);
+// console.log(process);               //how the prject is done like complete information execution steps are here 
+
+
+const parsedata=path1.parse(filepath);
+const resolvedata=path1.resolve(filepath);
+const dirdata=path1.dirname (filepath);
+const basedata=path1.basename(filepath);
+const extdata=path1.extname(filepath);
